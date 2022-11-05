52e616a0433c11dc866e78cff101bb1b257c0887
imahdimir <imahdimir@gmail.com>
2022-11-05 15:01:53 +0330

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# updates(clone) the code for removing empty Todoist sections in the daily
+#   routine project then exexutes it.
+pyenv virtualenv auto
+
+pyenv activate auto
+
+
+
