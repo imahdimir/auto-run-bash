d3b321c905b42dcfd3bdb23265c5ca4adbf0a934
imahdimir <imahdimir@gmail.com>
2022-11-05 15:38:51 +0330

    added finding the url

--- main.sh
+++ main.sh
@@ -6,5 +6,8 @@ pyenv virtualenv auto
 
 pyenv activate auto
 
+tar_url=$(curl \
+  -H "Accept: application/vnd.github+json" \
+  https://api.github.com/repos/imahdimir/auto-run/releases/latest | python -c 'import  json,sys;result=json.load(sys.stdin); print(result["tarball_url"])';)
 
 
