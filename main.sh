6d4258194ea1101805472e2ef284f9d6c552c733
imahdimir <imahdimir@gmail.com>
2022-11-07 19:56:47 +0330

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,14 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 
-# updates(clone) the code for removing empty Todoist sections in the daily
-#   routine project then exexutes it.
-# 
-# requirements: 
-#   1. pyenv
-#   2. pyenv-virtualenv
-#
-# Assumptions:
-#   1. main.sh would be downloaded in a dir by the dlmain.sh
+# Reads conf.json in PWD, gets the latest release of the target GitHub repo then
+#   executes the target python script.
+
 
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 eval "$(pyenv init -)"
@@ -16,57 +10,49 @@ eval "$(pyenv virtualenv-init -)"
 
 ipwd=$PWD
 
-echo -e "$fst changing dir to the parent dir\n"
-cd ..
-echo -e "$fst PWD is now $PWD"
-
-echo -e "$fst Make the new venv and return its name\n"
+echo -e "$fst Make a new venv and return its name\n"
 venv=$(python -c 'from autorunpy import auto; auto.make_venv();')
-echo -e "$fst venv: $venv"
+echo -e "$fst The new venv name: $venv\n"
 
 echo -e "$fst Download latset release of the conf target GitHub repo and ret its local dirpath\n"
-dirn=$(python -c 'from autorunpy import auto; auto.ret_dirn();')
-echo -e "The dirpath: $dirn"
+dirp=$(python -c 'from autorunpy import auto; auto.ret_dirp();')
+echo -e "The dirpath: $dirp"
 
 echo -e "$fst Return the module name to run from conf.json\n"
 m2r=$(python -c 'from autorunpy import auto; auto.ret_module_2_run_name();')
-echo -e "$fst Python module to running: $m2r"
+echo -e "$fst Python module to running: $m2r\n"
 
 echo -e "$fst Deactivate $av venv\n"
 pyenv deactivate $av
 
-echo -e "$fst cd to $dirn\n"
-cd $dirn
-echo -e "$fst PWD is now $PWD"
-
 echo -e "$fst Activating the new venv: $venv\n"
 pyenv activate $venv
 
-echo -e "$fst Installing dependencies from requirements.txt\n"
+echo -e "$fst Install reqs from requirements.txt in the new $venv venv\n"
 pyenv exec pip install --upgrade pip
 pyenv exec pip install -r requirements.txt
 
-echo -e "$fst Execute the target module $m2r using the $venv venv\n "
-pyenv exec python3 $m2r
+echo -e "$fst cd to $dirp\n"
+cd $dirp
+echo -e "$fst PWD is now $PWD\n"
 
-echo -e "$fst cd back to the parent dir\n"
-cd ..
-echo -e "$fst PWD is now $PWD"
+echo -e "$fst Execute the target module $m2r using the $venv venv\n"
+pyenv exec python3 $m2r
 
 echo -e "$fst Deactivate the new venv: $venv\n"
 pyenv deactivate $venv
 
-echo -e "$fst Re-activate $av venv"
+echo -e "$fst Re-activate $av venv\n"
 pyenv activate $av
 
-python -c 'from autorunpy import auto; auto.rm_venv();'
+echo -e "$fst Delete [If said so in conf] new created venv : $venv\n"
+pyenv exec python3 -c 'from autorunpy import auto; auto.rm_venv();'
 
-echo -e "$fst rm the new dir which contains the code: $dirn\n"
-rm -r $dirn
+echo -e "$fst rm the new dir which contains the code: $dirp\n"
+rm -r $dirp
 
-echo -e "$fst cd to initial dir $ipwd"
+echo -e "$fst cd to initial dir $ipwd\n"
 cd $ipwd
-
 
 
 
