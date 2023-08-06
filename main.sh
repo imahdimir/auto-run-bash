a38c56b4b76e22b4ff11e2ec1fbeac6e6f7d5a6e
imahdimir <imahdimir@gmail.com>
2023-08-06 16:31:46 +0330

    update

--- main.sh
+++ main.sh
@@ -6,27 +6,36 @@
 # requirements: 
 #   1. git
 
-export fst="\n  LOG:"
 
+# keep log as a variable
+export fst="log : "
+
+
+# cd to current dir 
 echo -e "$fst cd to self dir: $0\n"
 cd $(dirname $0)
 
-echo -e "$fst Self Update (runner bash script)\n"
+# update current dir from GitHub
+echo -e "$fst Self update (runner bash script)\n"
+
 git fetch --all
 git reset --hard origin/main
 
+
 cfn=$(echo "$1" | sed -E "s/.+\/([^\/]+)$/\1/")
 
-bash m1.sh $@
+echo $cfn
 
-if [ $? -eq 0 ]; then
-   echo OK
-   echo "" | mail -s "OK - $hostname - $cfn" $MAILTO
+# bash m1.sh $@
 
-else
-   echo FAIL
-   echo "" | mail -s "FAIL - $hostname - $cfn" $MAILTO
+# if [ $? -eq 0 ]; then
+#    echo OK
+#    echo "" | mail -s "OK - $hostname - $cfn" $MAILTO
 
-fi
+# else
+#    echo FAIL
+#    echo "" | mail -s "FAIL - $hostname - $cfn" $MAILTO
 
-echo -e "\n\n\t\t\t***   FINISHED   ***\n\n"
+# fi
+
+# echo -e "\n\n\t\t\t***   FINISHED   ***\n\n"
