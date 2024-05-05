d41b28f0ce369d6cb6f30545617d530e85d48eab
imahdimir <imahdimir@gmail.com>
2024-05-04 19:47:05 -0700

    Update setup_server.sh

--- setup_server.sh
+++ setup_server.sh
@@ -3,36 +3,23 @@
 # Install git
 sudo apt-get install git-all
 
-git clone https://github.com/imahdimir/auto_run_bash
-git clone https://github.com/imahdimir/auto_run_configs
-
 # Install pip3
 apt install python3-pip
 
-# pyenv reqs
-apt-get update
-apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-
-# Install pyenv
-curl https://pyenv.run | bash
-
-# Add pyenv to PATH
-echo 'export PATH="$(pyenv root)/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+# Install pyenv, use Its installation guide on GitHub
+    # pyenv reqs
+    # Install pyenv virtualenv it is now included in pyenv installation, make sure it is installed
+    # Add pyenv root to PATH and eval it on basrch, read the guide
 
 # Restart shell
 exec $SHELL
 
-# Install pyenv virtualenv plugin
-git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
-
-# add to .bashrc
-echo "eval "$(pyenv virtualenv-init -)"" >> ~/.bashrc
-
-exec $SHELL
+# clone auto run tool and configs
+git clone https://github.com/imahdimir/auto_run_bash
+git clone https://github.com/imahdimir/auto_run_configs
 
 # set tz
 timedatectl set-timezone America/Los_Angeles
 
-# touch .gt in $HOME
+# copy .gt.json to HOME
 # config cron
