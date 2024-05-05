f831bb2decfdb3bab6c2b7bc013abb8ca282ef6b
imahdimir <imahdimir@gmail.com>
2024-05-05 00:33:48 -0700

    update

--- s0.sh
+++ s0.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 
 << doc
-Updates itself (especially the next bash script),
-then passes entered arguments to the next bash script.
-
-requirements: 
+reqs: 
     - git
 doc
 
-cd "$HOME/auto_run_bash"
-
-# self update
+# Update Configs
+cd "$HOME/auto_run_configs/"
 git fetch --all -q
 git reset --hard origin/main -q
 
-# run the next bash script with the same arguments (all arguments @)
+# Self Update
+cd "$HOME/auto_run_bash"
+git fetch --all -q
+git reset --hard origin/main -q
+# Run Next Script, Same Args (All Args @)
 bash s1.sh $@
 
-# evaluate the exit code of the previous command to see if it's successful or not
-## get the config file name from the second argument
-# cfn=$(echo "$1" | sed -E "s/.+\/([^\/]+)$/\1/")
 if [ $? -eq 0 ]; then
     echo "DONE"
-    # echo "DONE." | mail -s "Done | $hostname | $cfn" $MAILTO
-
 else
-    echo "FAILED"
     echo $?
-    # echo "FAILED." | mail -s "FAILED | $hostname | $cfn" $MAILTO
 fi
--- s1.sh
+++ s1.sh
@@ -8,17 +8,17 @@ Requirements:
     - pyenv-virtualenv
 
 arguements:
-    - conf.json path
+    - conf.json stem
 doc
 
-# add pyenv to PATH (crontab doesn't have it)
+# + pyenv to PATH (crontab doesn't have it)
 export PATH="$(pyenv root)/bin:$PATH"
 
-# constants
+# Consts
 pyv=3.12.3
 av=autorunpy
 
-# following lines are needed for pyenv to work properly, to deactivate the venv properly
+# following lines are needed for pyenv to work properly
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
@@ -34,23 +34,13 @@ pyenv activate $av &> /dev/null
 # upgrade pip and autorunpy package
 pyenv exec pip install --upgrade pip autorunpy -q
 
-# change dir to the auto-run-configs repo dir, assumed the same dir as auto-run-bash
-cd "$HOME/auto_run_configs/"
-
-# update run configs
-git fetch --all -q
-git reset --hard origin/main -q
-
-# cd to the home dir
-cd
-
 # make a new environment and return its name
 venv=$(pyenv exec python -m autorunpy.make_venv $1)
 echo "venv: $venv"
 
 # return pip package name from conf.json
 pkg=$(pyenv exec python -m autorunpy.ret_pkg_name $1)
-# echo "pkg: $pkg"
+echo "pkg: $pkg"
 
 # return target module name to run in the targe repo
 m2r=$(pyenv exec python -m autorunpy.ret_module_2_run $1)
@@ -67,7 +57,7 @@ pyenv exec pip install --upgrade pip -q
 pyenv exec pip install $pkg -q
 
 # Execute the target module
-# echo "module to run: $m2r"
+echo "module to run: $m2r"
 pyenv exec python3 -m $m2r
 
 # Deactivate the new venv
